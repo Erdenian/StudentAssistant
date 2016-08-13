225110e52c3d9f839427c77eae6f65e9b2e783f8
Ilya Solovyev <erdenian@gmail.com>
2016-08-13 16:08:19 +0700

    AlarmActivity improvements

--- studentassistant/src/main/java/ru/erdenian/studentassistant/activities/AlarmActivity.java
+++ studentassistant/src/main/java/ru/erdenian/studentassistant/activities/AlarmActivity.java
@@ -1,13 +1,23 @@
 package ru.erdenian.studentassistant.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TimePicker;
 
 import ru.erdenian.studentassistant.R;
+import ru.erdenian.studentassistant.classes.Utils;
+
+/**
+ * Created by Erdenian on 13.08.2016.
+ */
 
 public class AlarmActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
     TimePicker tpTime;
 
     @Override
@@ -15,6 +25,20 @@ public class AlarmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
-        tpTime = (TimePicker) findViewById(R.id.aa_time_picker);
+        toolbar = (Toolbar) findViewById(R.id.ta_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.alarm);
+
+        drawerLayout = Utils.initializeNavigationView(getResources(), toolbar, this);
+
+        tpTime = (TimePicker) findViewById(R.id.ca_time_picker);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
