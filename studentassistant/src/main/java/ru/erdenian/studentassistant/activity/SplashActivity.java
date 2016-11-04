a955e3a08e02fd25e313aef071703a9b377a1da7
Ilya Solovyev <erdenian@gmail.com>
2016-11-04 14:29:48 +0300

    Начальный коммит

---
+++ studentassistant/src/main/java/ru/erdenian/studentassistant/activity/SplashActivity.java
@@ -0,0 +1,24 @@
+package ru.erdenian.studentassistant.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity, открывающееся при запуске приложения.
+ * Показывает картинку на весь экран, пока подгружается следующее Activity.
+ *
+ * @author Ilya Solovyev
+ * @version 0.0.0
+ * @since 0.0.0
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();*/
+    }
+}
