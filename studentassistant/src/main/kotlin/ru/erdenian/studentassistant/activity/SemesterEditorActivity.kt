f189f01bd0eb62ad48fa4c79f89b7e344db98b30
Ilya Solovyev <mail@erdenian.ru>
2016-11-28 15:55:45 +0300

    Данные в редакторе семестра теперь сохраняются по кнопке в меню
    Немного изменен интерфейс редактора семестра.
    В редакторе семестра данные теперь сохраяются при пересоздании Activity.
    Добавлена иконка check.xml.
    Из ScheduleManager пока убран поиск выбраного семестра при обновлении расписания.

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/activity/SemesterEditorActivity.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/activity/SemesterEditorActivity.kt
@@ -4,36 +4,40 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment
 import kotlinx.android.synthetic.main.content_semester_editor.*
-import kotlinx.android.synthetic.main.content_semester_editor.view.*
 import kotlinx.android.synthetic.main.toolbar.*
+import org.jetbrains.anko.toast
 import org.joda.time.LocalDate
 import ru.erdenian.studentassistant.R
+import ru.erdenian.studentassistant.extensions.getCompatColor
+import ru.erdenian.studentassistant.extensions.setColor
 import ru.erdenian.studentassistant.extensions.showDatePicker
-import ru.erdenian.studentassistant.schedule.OnScheduleUpdateListener
 import ru.erdenian.studentassistant.schedule.ScheduleManager
 import ru.erdenian.studentassistant.schedule.Semester
 
 class SemesterEditorActivity : AppCompatActivity(),
         View.OnClickListener,
         CalendarDatePickerDialogFragment.OnDateSetListener,
-        TextWatcher,
-        OnScheduleUpdateListener {
+        TextWatcher {
 
     companion object {
-        val SEMESTER_ID = "semester_id"
-        private val FIRST_DAY_TAG = "first_day_tag"
-        private val LAST_DAY_TAG = "last_day_tag"
+        const val SEMESTER_ID = "semester_id"
+
+        private const val FIRST_DAY = "first_day"
+        private const val LAST_DAY = "last_day"
+
+        private const val FIRST_DAY_TAG = "first_day_tag"
+        private const val LAST_DAY_TAG = "last_day_tag"
     }
 
     private val semester: Semester? by lazy { ScheduleManager[intent.getLongExtra(SEMESTER_ID, -1)] }
 
-    private lateinit var semestersNames: List<String>
+    private val semestersNames: List<String> by lazy { ScheduleManager.semestersNames.filter { it != semester?.name } }
 
-    private lateinit var name: String
     private var firstDay: LocalDate? = null
     private var lastDay: LocalDate? = null
 
@@ -44,42 +48,59 @@ class SemesterEditorActivity : AppCompatActivity(),
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        if (semester == null) supportActionBar!!.title = getString(R.string.title_activity_semester_editor_new_semester)
+
         content_semester_editor_semester_name_edit_text.addTextChangedListener(this)
         content_semester_editor_first_day.setOnClickListener(this)
         content_semester_editor_last_day.setOnClickListener(this)
 
-        name = semester?.name ?: ""
         firstDay = semester?.firstDay
         lastDay = semester?.lastDay
+
+        if (semester?.name != null) content_semester_editor_semester_name_edit_text.setText(semester!!.name)
     }
 
-    override fun onStart() {
-        super.onStart()
-        ScheduleManager.setOnScheduleUpdateListener(this)
-        onScheduleUpdate()
-        invalidateViews()
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(FIRST_DAY, firstDay.toString())
+        outState.putString(LAST_DAY, lastDay.toString())
+        super.onSaveInstanceState(outState)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (!content_semester_editor_semester_name.isErrorEnabled && (firstDay != null) && (lastDay != null))
-            ScheduleManager.addSemester(semester?.copy(name = name, firstDay = firstDay!!, lastDay = lastDay!!) ?:
-                    Semester(name, firstDay!!, lastDay!!))
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        val firstDayString = savedInstanceState.getString(FIRST_DAY)
+        val lastDayString = savedInstanceState.getString(LAST_DAY)
+
+        if (firstDayString != "null") {
+            firstDay = LocalDate.parse(firstDayString)
+            content_semester_editor_first_day.text = firstDay?.toString() ?: getString(R.string.activity_semester_editor_first_day)
+        }
+        if (lastDayString != "null") {
+            lastDay = LocalDate.parse(lastDayString)
+            content_semester_editor_last_day.text = lastDay?.toString() ?: getString(R.string.activity_semester_editor_last_day)
+        }
+
+        super.onRestoreInstanceState(savedInstanceState)
     }
 
-    override fun onScheduleUpdate() {
-        semestersNames = ScheduleManager.semestersNames.filter { it != semester?.name }
-    }
-
-    fun invalidateViews() {
-        content_semester_editor_semester_name_edit_text.setText(name)
-        content_semester_editor_first_day.text = firstDay?.toString() ?: getString(R.string.activity_semester_editor_first_day)
-        content_semester_editor_last_day.text = lastDay?.toString() ?: getString(R.string.activity_semester_editor_last_day)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_editor, menu)
+        menu.setColor(getCompatColor(R.color.action_bar_icons_color))
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> finish()
+            R.id.menu_editor_save -> {
+                if (content_semester_editor_semester_name_edit_text.text.isEmpty()) {
+                    toast(R.string.activity_semester_editor_error_empty_name)
+                } else if (!content_semester_editor_semester_name.isErrorEnabled && (firstDay != null) && (lastDay != null)) {
+                    val name = content_semester_editor_semester_name_edit_text.text.toString()
+                    ScheduleManager.addSemester(semester?.copy(name = name, firstDay = firstDay!!, lastDay = lastDay!!) ?:
+                            Semester(name, firstDay!!, lastDay!!))
+                    finish()
+                }
+            }
             else -> throw IllegalArgumentException("Неизвестный id: ${item.itemId}")
         }
         return super.onOptionsItemSelected(item)
@@ -95,12 +116,8 @@ class SemesterEditorActivity : AppCompatActivity(),
         with(content_semester_editor_semester_name) {
             isErrorEnabled = true
 
-            if (s!!.isEmpty()) error = getString(R.string.activity_semester_editor_error_empty_name)
-            else if (semestersNames.contains(s.toString())) error = getString(R.string.activity_semester_editor_error_name_not_avaliable)
-            else {
-                isErrorEnabled = false
-                name = content_semester_editor_semester_name_edit_text.text.toString()
-            }
+            if (semestersNames.contains(s.toString())) error = getString(R.string.activity_semester_editor_error_name_not_avaliable)
+            else isErrorEnabled = false
         }
     }
 
@@ -117,10 +134,15 @@ class SemesterEditorActivity : AppCompatActivity(),
     override fun onDateSet(dialog: CalendarDatePickerDialogFragment, year: Int, monthOfYear: Int, dayOfMonth: Int) {
         val newDate = LocalDate(year, monthOfYear + 1, dayOfMonth)
         when (dialog.tag) {
-            FIRST_DAY_TAG -> firstDay = newDate
-            LAST_DAY_TAG -> lastDay = newDate
+            FIRST_DAY_TAG -> {
+                firstDay = newDate
+                content_semester_editor_first_day.text = firstDay!!.toString()
+            }
+            LAST_DAY_TAG -> {
+                lastDay = newDate
+                content_semester_editor_last_day.text = lastDay!!.toString()
+            }
             else -> throw IllegalArgumentException("Неизвестный тэг: ${dialog.tag}")
         }
-        invalidateViews()
     }
 }
