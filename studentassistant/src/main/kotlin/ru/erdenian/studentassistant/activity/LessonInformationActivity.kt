28ae9a6c6a02b79167af35909c303dc56fb65f2f
Ilya Solovyev <mail@erdenian.ru>
2016-12-23 06:22:51 +0300

    Исправлен стиль даты в карточках дз

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/activity/LessonInformationActivity.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/activity/LessonInformationActivity.kt
@@ -63,7 +63,7 @@ class LessonInformationActivity : AppCompatActivity() {
 
             (card.findViewById(R.id.card_homework_subject_name) as TextView).text = it.subjectName
             (card.findViewById(R.id.card_homework_description) as TextView).text = it.description
-            (card.findViewById(R.id.card_homework_deadline) as TextView).text = it.deadline.toString()
+            (card.findViewById(R.id.card_homework_deadline) as TextView).text = it.deadline.toString("dd.MM.yyyy")
 
             val homeworkId = it.id
 
