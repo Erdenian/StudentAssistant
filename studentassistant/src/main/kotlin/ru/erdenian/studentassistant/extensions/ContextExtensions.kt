bd343aca62eee5eb9561d637d0a50f244a70c652
Ilya Solovyev <mail@erdenian.ru>
2017-07-14 15:20:37 +0300

    Добавлен метод для очистки данных приложения

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ContextExtensions.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ContextExtensions.kt
@@ -2,5 +2,18 @@ package ru.erdenian.studentassistant.extensions
 
 import android.content.Context
 import android.support.v4.content.ContextCompat
+import java.io.File
 
 fun Context.getCompatColor(id: Int) = ContextCompat.getColor(this, id)
+
+fun Context.clearApplicationData() {
+
+  fun deleteFile(file: File) {
+    if (file.isDirectory) file.list().forEach { deleteFile(File(file, it)) }
+    else file.delete()
+  }
+
+  val applicationDirectory = File(cacheDir.parent)
+  if (applicationDirectory.exists())
+    applicationDirectory.list().filter { it != "lib" }.forEach { deleteFile(File(applicationDirectory, it)) }
+}
