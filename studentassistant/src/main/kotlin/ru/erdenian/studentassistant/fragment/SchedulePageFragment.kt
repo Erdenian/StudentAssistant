cbc426841454c3cf3d1583aa5518ce8a6d25e850
Илья Соловьёв <mail@erdenian.ru>
2017-07-18 17:47:33 +0700

    Рефакторинг SchedulePageFragment

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/fragment/SchedulePageFragment.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/fragment/SchedulePageFragment.kt
@@ -27,28 +27,20 @@ class SchedulePageFragment : Fragment() {
     private const val PAGE_WEEKDAY = "page_weekday"
     private const val SHOW_WEEKS_AND_DATES = "show_weeks_and_dates"
 
-    fun newInstance(semesterId: Long, date: LocalDate): SchedulePageFragment {
-      val schedulePageFragment = SchedulePageFragment()
-      val arguments = Bundle()
-      with(arguments) {
+    fun newInstance(semesterId: Long, date: LocalDate): SchedulePageFragment = SchedulePageFragment().apply {
+      arguments = Bundle().apply {
         putLong(PAGE_SEMESTER_ID, semesterId)
         putString(PAGE_DATE, date.toString())
         putBoolean(SHOW_WEEKS_AND_DATES, false)
       }
-      schedulePageFragment.arguments = arguments
-      return schedulePageFragment
     }
 
-    fun newInstance(semesterId: Long, weekday: Int): SchedulePageFragment {
-      val schedulePageFragment = SchedulePageFragment()
-      val arguments = Bundle()
-      with(arguments) {
+    fun newInstance(semesterId: Long, weekday: Int): SchedulePageFragment = SchedulePageFragment().apply {
+      arguments = Bundle().apply {
         putLong(PAGE_SEMESTER_ID, semesterId)
-        arguments.putInt(PAGE_WEEKDAY, weekday)
+        putInt(PAGE_WEEKDAY, weekday)
         putBoolean(SHOW_WEEKS_AND_DATES, true)
       }
-      schedulePageFragment.arguments = arguments
-      return schedulePageFragment
     }
   }
 
@@ -57,36 +49,26 @@ class SchedulePageFragment : Fragment() {
   private val day: LocalDate by lazy { LocalDate(arguments.getString(PAGE_DATE)) }
   private val weekday: Int by lazy { arguments.getInt(PAGE_WEEKDAY, -1) }
 
-  override fun onCreate(savedInstanceState: Bundle?) = super.onCreate(savedInstanceState)
-
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-    val lessons = if (showWeeksAndDates)
-      ScheduleManager.getLessons(semesterId, weekday)
-    else
-      ScheduleManager.getLessons(semesterId, day)
+    val lessons = if (showWeeksAndDates) ScheduleManager.getLessons(semesterId, weekday)
+    else ScheduleManager.getLessons(semesterId, day)
 
-    if (lessons.isEmpty()) {
-      return inflater.inflate(R.layout.fragment_free_day, container, false)
+    return if (lessons.isEmpty()) inflater.inflate(R.layout.fragment_free_day, container, false)
+    else (inflater.inflate(R.layout.list_view, container, false) as ListView).apply {
+      adapter = ScheduleListAdapter(context, semesterId, showWeeksAndDates, lessons)
+      if (showWeeksAndDates) registerForContextMenu(this)
     }
-
-    val view = inflater.inflate(R.layout.list_view, container, false) as ListView
-    view.adapter = ScheduleListAdapter(context, semesterId, showWeeksAndDates, lessons)
-    if (showWeeksAndDates) registerForContextMenu(view)
-
-    return view
   }
 
   override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
     super.onCreateContextMenu(menu, v, menuInfo)
     activity.menuInflater.inflate(R.menu.context_menu_schedule_page_fragment, menu)
-
-    val info = menuInfo as AdapterView.AdapterContextMenuInfo
-    menu.setHeaderTitle(ScheduleManager.getLesson(semesterId, info.id)!!.subjectName)
+    menu.setHeaderTitle(ScheduleManager.getLesson(semesterId, (menuInfo as AdapterView.AdapterContextMenuInfo).id).subjectName)
   }
 
   override fun onContextItemSelected(item: MenuItem): Boolean {
     val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
-    val lesson = ScheduleManager.getLesson(semesterId, info.id)!!
+    val lesson = ScheduleManager.getLesson(semesterId, info.id)
     when (item.itemId) {
       R.id.context_menu_schedule_page_fragment_copy -> context.startActivity<LessonEditorActivity>(
           context.SEMESTER_ID to semesterId,
@@ -99,23 +81,21 @@ class SchedulePageFragment : Fragment() {
           //context.startService<ScheduleService>()
         }
 
-        if (ScheduleManager.getHomeworks(semesterId, lesson.subjectName).isNotEmpty()
-            && (ScheduleManager.getLessons(semesterId, lesson.subjectName).size == 1)) {
-
+        if (ScheduleManager.getHomeworks(semesterId, lesson.subjectName).isNotEmpty() &&
+            (ScheduleManager.getLessons(semesterId, lesson.subjectName).size == 1)) {
           context.alert(R.string.activity_lesson_editor_alert_delete_homeworks_message,
               R.string.activity_lesson_editor_alert_delete_homeworks_title) {
             positiveButton(R.string.activity_lesson_editor_alert_delete_homeworks_yes) { remove() }
             negativeButton(R.string.activity_lesson_editor_alert_delete_homeworks_cancel) {}
           }.show()
         } else {
-
           context.alert(R.string.activity_lesson_editor_alert_delete_message) {
             positiveButton(R.string.activity_lesson_editor_alert_delete_yes) { remove() }
             negativeButton(R.string.activity_lesson_editor_alert_delete_no) {}
           }.show()
         }
       }
-      else -> return super.onContextItemSelected(item)
+      else -> throw IllegalArgumentException("Неизвестный id: ${item.itemId}")
     }
     return true
   }
