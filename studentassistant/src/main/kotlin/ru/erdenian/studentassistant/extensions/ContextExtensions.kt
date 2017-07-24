5982e7db2c372c5567713595728f2c41df3ae135
Илья Соловьёв <mail@erdenian.ru>
2017-07-24 21:41:23 +0700

    Добавлена документация для CollectionExtensions, ContextExtensions и FragmentActivityExtensions
    Так же небольшой рефакторинг FragmentActivityExtensions.

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ContextExtensions.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ContextExtensions.kt
@@ -4,8 +4,22 @@ import android.content.Context
 import android.support.v4.content.ContextCompat
 import java.io.File
 
+/**
+ * Обертка над [ContextCompat.getColor].
+ *
+ * @author Ilya Solovyev
+ * @return цвет
+ * @param id id цвета
+ * @since 0.0.0
+ */
 fun Context.getCompatColor(id: Int) = ContextCompat.getColor(this, id)
 
+/**
+ * Очищает данные приложения.
+ *
+ * @author Ilya Solovyev
+ * @since 0.2.6
+ */
 fun Context.clearApplicationData() {
 
   fun deleteFile(file: File) {
