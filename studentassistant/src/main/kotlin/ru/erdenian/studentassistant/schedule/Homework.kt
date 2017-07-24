002a474f68b1cdc08bc5f04d4793e346b4257410
Илья Соловьёв <mail@erdenian.ru>
2017-07-24 22:52:18 +0700

    Добавлена документация для Homework, подправлена для Functions

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/schedule/Homework.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/schedule/Homework.kt
@@ -3,6 +3,17 @@ package ru.erdenian.studentassistant.schedule
 import com.google.common.collect.ComparisonChain
 import org.joda.time.LocalDate
 
+/**
+ * Класс домашнего задания.
+ *
+ * @author Ilya Solovyev
+ * @since 0.0.0
+ * @property subjectName название предмета, по которому задано занание
+ * @property description описание задания
+ * @property deadline срок сдачи
+ * @property id уникальный id задания
+ * @throws IllegalArgumentException если [subjectName] или [description] пусты
+ */
 data class Homework(val subjectName: String, val description: String, val deadline: LocalDate,
                     val id: Long = generateId()) : Comparable<Homework> {
 
