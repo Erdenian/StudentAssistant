888b0ac09e4757bf03743c970f3c3b9a6e1ee92f
Илья Соловьёв <mail@erdenian.ru>
2017-07-24 17:54:25 +0700

    Добавлена документация для Functions.kt

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/schedule/Functions.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/schedule/Functions.kt
@@ -1,3 +1,11 @@
 package ru.erdenian.studentassistant.schedule
 
+/**
+ * Возвращает long, который можно использовать как id.
+ *
+ * @return id
+ * @author Ilya Solovyev
+ * @version 2
+ * @since 0.2.0
+ */
 fun generateId() = System.currentTimeMillis()
