51c17534f7fd4846415c165e10c03ba9154c503a
Илья Соловьёв <mail@erdenian.ru>
2017-07-24 21:44:41 +0700

    Переименованы параметры preselected в preselectedDate и preselectedTime

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/FragmentActivityExtensions.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/FragmentActivityExtensions.kt
@@ -17,19 +17,19 @@ import java.util.*
  * @param onDateSetListener обработчик результата выбора
  * @param firstDay первый день промежутка (если null, используется 1 января 1900)
  * @param lastDay последний день промежутка (если null, используется 31 декабря 2100)
- * @param preselected изначально выбранный день (если null, используется текущая дата)
+ * @param preselectedDate изначально выбранный день (если null, используется текущая дата)
  * @param tag тэг, который будет передан в [onDateSetListener]
  * @since 0.0.0
  */
 fun FragmentActivity.showDatePicker(onDateSetListener: CalendarDatePickerDialogFragment.OnDateSetListener,
                                     firstDay: LocalDate? = null, lastDay: LocalDate? = null,
-                                    preselected: LocalDate? = LocalDate.now(), tag: String = "date_picker") {
+                                    preselectedDate: LocalDate? = LocalDate.now(), tag: String = "date_picker") {
   CalendarDatePickerDialogFragment().apply {
     firstDayOfWeek = Calendar.MONDAY
     setThemeCustom(R.style.DatePicker)
     setDateRange(firstDay?.toCalendarDay(), lastDay?.toCalendarDay())
     setOnDateSetListener(onDateSetListener)
-    preselected?.run { setPreselectedDate(year, monthOfYear - 1, dayOfMonth) }
+    preselectedDate?.run { setPreselectedDate(year, monthOfYear - 1, dayOfMonth) }
   }.show(supportFragmentManager, tag)
 }
 
@@ -38,14 +38,14 @@ fun FragmentActivity.showDatePicker(onDateSetListener: CalendarDatePickerDialogF
  *
  * @author Ilya Solovyev
  * @param onTimeSetListener обработчик результата выбора
- * @param preselected изначально выбранное время (если null, используется текущее время)
+ * @param preselectedTime изначально выбранное время (если null, используется текущее время)
  * @param tag тэг, который будет передан в [onTimeSetListener]
  * @since 0.0.0
  */
 fun FragmentActivity.showTimePicker(onTimeSetListener: RadialTimePickerDialogFragment.OnTimeSetListener,
-                                    preselected: LocalTime? = null, tag: String = "time_picker") {
+                                    preselectedTime: LocalTime? = null, tag: String = "time_picker") {
   RadialTimePickerDialogFragment().apply {
     setOnTimeSetListener(onTimeSetListener)
-    preselected?.run { setStartTime(hourOfDay, minuteOfHour) }
+    preselectedTime?.run { setStartTime(hourOfDay, minuteOfHour) }
   }.show(supportFragmentManager, tag)
 }
