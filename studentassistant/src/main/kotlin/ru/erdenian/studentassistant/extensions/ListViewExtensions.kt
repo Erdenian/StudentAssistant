401e633512f2258ee624672485e834f04653b403
Илья Соловьёв <mail@erdenian.ru>
2017-07-24 21:51:29 +0700

    Добавлена документация для ListViewExtensions

--- studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ListViewExtensions.kt
+++ studentassistant/src/main/kotlin/ru/erdenian/studentassistant/extensions/ListViewExtensions.kt
@@ -2,6 +2,12 @@ package ru.erdenian.studentassistant.extensions
 
 import android.widget.ListView
 
+/**
+ * Позиция скролла.
+ *
+ * @author Ilya Solovyev
+ * @since 0.0.0
+ */
 var ListView.scrollPosition: Pair<Int, Int>
   get() = Pair(firstVisiblePosition, (getChildAt(0)?.top ?: paddingTop) - paddingTop)
   set(value) = setSelectionFromTop(value.first, value.second)
