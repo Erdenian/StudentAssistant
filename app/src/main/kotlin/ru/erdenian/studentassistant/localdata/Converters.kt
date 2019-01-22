2d70abf14a7b3e3fffa7ac33adad7eaa0be78b10
Ilya Solovyev <mail@erdenian.ru>
2019-01-22 15:38:00 +0300

    Добавлены конвертеры

---
+++ app/src/main/kotlin/ru/erdenian/studentassistant/localdata/Converters.kt
@@ -0,0 +1,65 @@
+package ru.erdenian.studentassistant.localdata
+
+import androidx.room.TypeConverter
+import org.joda.time.Days
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
+import ru.erdenian.studentassistant.extensions.toImmutableSortedSet
+import ru.erdenian.studentassistant.schedule.LessonRepeat
+
+class Converters {
+
+    private val epoch = LocalDate(1970, 1, 1)
+    private val separator = ";"
+
+    @TypeConverter
+    fun localDateToInt(value: LocalDate?): Int? =
+        if (value == null) null else Days.daysBetween(epoch, value).days
+
+    @TypeConverter
+    fun intToLocalDate(value: Int?): LocalDate? =
+        if (value == null) null else epoch.plusDays(value)
+
+    @TypeConverter
+    fun localTimeToInt(value: LocalTime?): Int? =
+        value?.millisOfDay
+
+    @TypeConverter
+    fun intToLocalTime(value: Int?): LocalTime? =
+        if (value == null) null else LocalTime.MIDNIGHT.plusMillis(value)
+
+    @TypeConverter
+    fun stringsToString(value: List<String?>?): String? =
+        value?.joinToString(separator)
+
+    @TypeConverter
+    fun stringToStrings(value: String?): List<String?>? =
+        value?.split(separator)
+
+    @TypeConverter
+    fun lessonRepeatToString(value: LessonRepeat?): String? =
+        if (value == null) null else when (value) {
+            is LessonRepeat.ByWeekday -> value.weekday.toString() +
+                    separator + value.weeks.joinToString(separator)
+            is LessonRepeat.ByDates -> value.dates.joinToString(separator)
+        }
+
+    @TypeConverter
+    fun stringToLessonRepeat(value: String?): LessonRepeat? =
+        if (value == null) null else {
+            if (value.contains('.')) LessonRepeat.ByDates(
+                value.split(separator).map {
+                    LocalDate.parse(
+                        it
+                    )
+                }.toImmutableSortedSet()
+            )
+            else {
+                val separated = value.split(separator)
+                LessonRepeat.ByWeekday(
+                    separated[0].toInt(),
+                    separated.drop(1).map { it.toBoolean() }
+                )
+            }
+        }
+}
