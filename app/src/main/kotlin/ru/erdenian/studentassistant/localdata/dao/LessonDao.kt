f34e6243544c46df378a43e63106360e1a737996
Ilya Solovyev <mail@erdenian.ru>
2019-01-22 16:57:57 +0300

    Добавлены методы для получения данных в LessonDao

--- app/src/main/kotlin/ru/erdenian/studentassistant/localdata/dao/LessonDao.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/localdata/dao/LessonDao.kt
@@ -1,8 +1,10 @@
 package ru.erdenian.studentassistant.localdata.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
+import org.joda.time.LocalDate
 import ru.erdenian.studentassistant.localdata.entity.LessonNew
+import ru.erdenian.studentassistant.localdata.entity.SemesterNew
+import ru.erdenian.studentassistant.schedule.LessonRepeat
 
 @Dao
 interface LessonDao {
@@ -11,7 +13,22 @@ interface LessonDao {
     fun insert(lesson: LessonNew)
 
     @Query("SELECT * FROM lessons WHERE semester_id = :semesterId ORDER BY start_time, end_time, _id")
-    fun get(semesterId: Long): LiveData<List<LessonNew>>
+    fun get(semesterId: Long): List<LessonNew>
+
+    @Query("SELECT * FROM lessons WHERE semester_id = :semesterId AND _id = :lessonId")
+    fun get(semesterId: Long, lessonId: Long): LessonNew?
+
+    fun get(semester: SemesterNew, day: LocalDate): List<LessonNew> {
+        val weekNumber = semester.getWeekNumber(day)
+        return get(semester.id).filter { it.lessonRepeat.repeatsOnDay(day, weekNumber) }
+    }
+
+    fun get(semesterId: Long, weekday: Int): List<LessonNew> = get(semesterId).filter {
+        (it.lessonRepeat as? LessonRepeat.ByWeekday)?.repeatsOnWeekday(weekday) ?: false
+    }
+
+    @Query("SELECT * FROM lessons WHERE semester_id = :semesterId AND subject_name = :subjectName ORDER BY start_time, end_time, _id")
+    fun get(semesterId: Long, subjectName: String): List<LessonNew>
 
     @Delete
     fun delete(lesson: LessonNew)
