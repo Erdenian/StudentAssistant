526a498a6d026bd5cadd102607008a9c89f4bda2
Ilya Solovyev <mail@erdenian.ru>
2019-01-24 13:30:34 +0300

    Поправлены связи

--- app/src/main/kotlin/ru/erdenian/studentassistant/localdata/dao/LessonDao.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/localdata/dao/LessonDao.kt
@@ -1,11 +1,8 @@
 package ru.erdenian.studentassistant.localdata.dao
 
 import androidx.room.*
-import org.joda.time.LocalDate
 import org.joda.time.Period
 import ru.erdenian.studentassistant.localdata.entity.LessonNew
-import ru.erdenian.studentassistant.localdata.entity.SemesterNew
-import ru.erdenian.studentassistant.schedule.LessonRepeat
 
 @Dao
 interface LessonDao {
@@ -23,14 +20,15 @@ interface LessonDao {
     @Query("SELECT * FROM lessons WHERE semester_id = :semesterId AND _id = :lessonId")
     fun get(semesterId: Long, lessonId: Long): LessonNew?
 
-    fun get(semester: SemesterNew, day: LocalDate): List<LessonNew> {
+    // Todo: вынести в Repository
+    /*fun get(semester: SemesterNew, day: LocalDate): List<LessonNew> {
         val weekNumber = semester.getWeekNumber(day)
         return get(semester.id).filter { it.lessonRepeat.repeatsOnDay(day, weekNumber) }
     }
 
     fun get(semesterId: Long, weekday: Int): List<LessonNew> = get(semesterId).filter {
         (it.lessonRepeat as? LessonRepeat.ByWeekday)?.repeatsOnWeekday(weekday) ?: false
-    }
+    }*/
 
     @Query("SELECT * FROM lessons WHERE semester_id = :semesterId AND subject_name = :subjectName ORDER BY start_time, end_time, _id")
     fun get(semesterId: Long, subjectName: String): List<LessonNew>
@@ -41,14 +39,19 @@ interface LessonDao {
     @Query("SELECT DISTINCT type FROM lessons WHERE semester_id = :semesterId ORDER BY type")
     fun getTypes(semesterId: Long): List<String>
 
+    // Todo
     @Query("SELECT DISTINCT teachers FROM lessons WHERE semester_id = :semesterId ORDER BY teachers")
     fun getTeachers(semesterId: Long): List<String>
 
+    // Todo
     @Query("SELECT DISTINCT classrooms FROM lessons WHERE semester_id = :semesterId ORDER BY classrooms")
     fun getClassrooms(semesterId: Long): List<String>
 
     @Query("SELECT IFNULL((SELECT end_time - start_time as duration FROM lessons WHERE semester_id = :semesterId GROUP BY duration ORDER BY COUNT(duration) desc), $DEFAULT_DURATION_MILLIS)")
     fun getLessonLength(semesterId: Long): Period
+
+    @Query("SELECT COUNT(_id) FROM lessons WHERE semester_id = :semesterId AND subject_name = :subjectName")
+    fun getCount(semesterId: Long, subjectName: String): Int
 
     @Query("UPDATE lessons SET subject_name = :newName WHERE semester_id = :semesterId AND subject_name = :oldName")
     fun renameSubject(semesterId: Long, oldName: String, newName: String)
