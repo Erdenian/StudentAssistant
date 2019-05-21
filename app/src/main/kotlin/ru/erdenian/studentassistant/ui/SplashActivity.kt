b4e615210b318c9ecbcada6c3a2749db6f250b23
Ilya Solovyev <mail@erdenian.ru>
2019-05-21 23:48:11 +0300

    SplashActivity перенесена в новый пакет ui

--- app/src/main/kotlin/ru/erdenian/studentassistant/ui/SplashActivity.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/SplashActivity.kt
@@ -1,18 +1,16 @@
-package ru.erdenian.studentassistant.activity
+package ru.erdenian.studentassistant.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.startService
-import ru.erdenian.studentassistant.localdata.ScheduleManager
+import ru.erdenian.studentassistant.activity.ScheduleActivity
 import ru.erdenian.studentassistant.service.ScheduleService
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        ScheduleManager.initialize(applicationContext)
 
         startService<ScheduleService>()
         startActivity<ScheduleActivity>()
