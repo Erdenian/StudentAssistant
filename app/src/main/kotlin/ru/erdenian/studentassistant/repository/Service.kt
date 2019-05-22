6e10b883a183bb3b893a2c2c5ca829dde9529629
Ilya Solovyev <mail@erdenian.ru>
2019-05-22 21:58:31 +0300

    Добавлен класс SortedList

--- app/src/main/kotlin/ru/erdenian/studentassistant/repository/Service.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/repository/Service.kt
@@ -3,7 +3,11 @@ package ru.erdenian.studentassistant.repository
 import java.io.Serializable
 import java.util.SortedSet
 
-class ImmutableSortedSet<E>(private val value: SortedSet<E>) : Set<E> by value, Serializable
+class ImmutableSortedSet<E : Comparable<E>>(
+    private val value: SortedSet<E>
+) : Set<E> by value, Serializable {
+    val list: SortedList<E> by lazy { value.toSortedList() }
+}
 
 /**
  * Преобразовывает коллекцию в [ImmutableSortedSet].
@@ -22,3 +26,23 @@ fun <E : Comparable<E>> Collection<E>.toImmutableSortedSet() = ImmutableSortedSe
  * @return ImmutableSortedSet, содержащий те же элементы, что и коллекция
  */
 fun <E : Comparable<E>> Sequence<E>.toImmutableSortedSet() = ImmutableSortedSet(toSortedSet())
+
+class SortedList<E : Comparable<E>>(private val value: List<E>) : List<E> by value.sorted()
+
+/**
+ * Преобразовывает коллекцию в [SortedList].
+ *
+ * @author Ilya Solovyev
+ * @since 0.3.0
+ * @return SortedList, содержащий те же элементы, что и коллекция
+ */
+fun <E : Comparable<E>> Collection<E>.toSortedList() = SortedList(toList())
+
+/**
+ * Преобразовывает [Sequence] в [SortedList].
+ *
+ * @author Ilya Solovyev
+ * @since 0.3.0
+ * @return SortedList, содержащий те же элементы, что и коллекция
+ */
+fun <E : Comparable<E>> Sequence<E>.toSortedList() = SortedList(toList())
