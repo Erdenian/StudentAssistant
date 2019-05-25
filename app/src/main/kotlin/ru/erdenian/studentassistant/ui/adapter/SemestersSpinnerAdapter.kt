9f685fa848d1a8de3e0960be9e0eea7c5e822887
Ilya Solovyev <mail@erdenian.ru>
2019-05-25 19:56:27 +0300

    Переписан SemestersAdapter

---
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/adapter/SemestersSpinnerAdapter.kt
@@ -0,0 +1,26 @@
+package ru.erdenian.studentassistant.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import ru.erdenian.studentassistant.R
+import ru.erdenian.studentassistant.repository.entity.SemesterNew
+
+class SemestersSpinnerAdapter(private val semesters: List<SemesterNew>) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View =
+        ((convertView ?: LayoutInflater.from(parent.context).inflate(
+            R.layout.spinner_item_semesters, parent, false
+        )) as TextView).apply { text = semesters[position].name }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View =
+        ((convertView ?: LayoutInflater.from(parent.context).inflate(
+            R.layout.spinner_dropdown_item_semesters, parent, false
+        )) as TextView).apply { text = semesters[position].name }
+
+    override fun getItem(position: Int) = semesters[position]
+    override fun getItemId(position: Int) = semesters[position].id
+    override fun getCount() = semesters.size
+}
