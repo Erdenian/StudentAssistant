44e667f29e73736435d35c9e3d978c8df8893687
Ilya Solovyev <mail@erdenian.ru>
2019-06-12 21:27:32 +0300

    CheckBoxWithText переписан на Kotlin

---
+++ customviews/src/main/java/ru/erdenian/studentassistant/customviews/CheckBoxWithText.kt
@@ -0,0 +1,85 @@
+package ru.erdenian.studentassistant.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.LinearLayout
+import android.widget.TextView
+
+/**
+ * Чекбокс с текстом под ним.
+ *
+ * @author Ilya Solovyev
+ * @version 1.0.0
+ * @see CheckBox
+ * @see TextView
+ * @since 0.2.6
+ */
+class CheckBoxWithText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val checkBox: CheckBox = findViewById(R.id.cwt_checkbox)
+    private val textView: TextView = findViewById(R.id.cwt_text)
+
+
+    /**
+     * Оборачивает [CheckBox.isChecked] и [CheckBox.setChecked]
+     *
+     * @since 0.2.6
+     */
+    var isChecked: Boolean
+        get() = checkBox.isChecked
+        set(checked) {
+            checkBox.isChecked = checked
+        }
+
+    /**
+     * Оборачивает [TextView.getText] и [TextView.setText]
+     *
+     * @since 0.2.6
+     */
+    var text: CharSequence?
+        get() = textView.text
+        set(text) {
+            textView.text = text
+        }
+
+    init {
+        orientation = VERTICAL
+        gravity = Gravity.CENTER_HORIZONTAL
+        inflate(context, R.layout.checkbox_with_text, this)
+
+        if (attrs != null) {
+            val typedArray = context.theme.obtainStyledAttributes(
+                attrs, R.styleable.CheckBoxWithText, defStyleAttr, 0
+            )
+            try {
+                isChecked = typedArray.getBoolean(R.styleable.CheckBoxWithText_checked, false)
+                text = typedArray.getString(R.styleable.CheckBoxWithText_text)
+            } finally {
+                typedArray.recycle()
+            }
+        }
+    }
+
+    /**
+     * Оборачивает [CheckBox.setOnCheckedChangeListener]
+     *
+     * @since 0.2.6
+     */
+    fun setOnCheckedChangeListener(listener: CompoundButton.OnCheckedChangeListener?) {
+        checkBox.setOnCheckedChangeListener(listener)
+    }
+
+    /**
+     * @since 0.2.6
+     */
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        checkBox.isEnabled = enabled
+        textView.isEnabled = enabled
+    }
+}
