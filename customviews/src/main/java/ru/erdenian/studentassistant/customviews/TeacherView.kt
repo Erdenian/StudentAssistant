cd20e8e6a7cfff58bb616a7bc171af8830a5cb98
Ilya Solovyev <mail@erdenian.ru>
2019-06-12 22:49:55 +0300

    Удалена разметка для CheckBoxWithText и TeacherView

--- customviews/src/main/java/ru/erdenian/studentassistant/customviews/TeacherView.kt
+++ customviews/src/main/java/ru/erdenian/studentassistant/customviews/TeacherView.kt
@@ -1,10 +1,14 @@
 package ru.erdenian.studentassistant.customviews
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.view.Gravity
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 
 /**
  * Иконка и имя преподавателя.
@@ -19,7 +23,10 @@ class TeacherView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val textView: TextView = findViewById(R.id.tv_name)
+    private val textView = AppCompatTextView(context).apply {
+        textSize = resources.getDimension(R.dimen.card_secondary_text_size)
+        setTextColor(ContextCompat.getColor(context, R.color.colorSecondaryText))
+    }
 
     /**
      * Оборачивает [TextView.getText] и [TextView.setText]
@@ -35,7 +42,15 @@ class TeacherView @JvmOverloads constructor(
     init {
         orientation = HORIZONTAL
         gravity = Gravity.BOTTOM
-        inflate(context, R.layout.teacher_view, this)
+
+        addView(AppCompatImageView(context).apply {
+            layoutParams = layoutParams.apply { gravity = Gravity.CENTER_VERTICAL }
+            setImageResource(R.drawable.ic_menu_account)
+            imageTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(context, R.color.colorSecondaryText)
+            )
+        })
+        addView(textView)
 
         if (attrs != null) {
             val typedArray = context.theme.obtainStyledAttributes(
