570c45f63e9f568a604c17bb38f0989f55edf521
Ilya Solovyev <mail@erdenian.ru>
2019-06-13 23:45:19 +0300

    Правки в build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,15 +83,14 @@ dependencies {
     val appcompat_version: String by project
     val cardview_version: String by project
 
-    val lifecycle_version: String by project
-    val navigation_version: String by project
+    val lifecycle_version = "2.2.0-alpha01"
+    //val navigation_version: String by project
     val room_version = "2.1.0-rc01"
 
-    val kodein_version = "6.2.1"
-    val retrofit_version = "2.6.0"
+    //val kodein_version = "6.2.1"
+    //val retrofit_version = "2.6.0"
 
     val joda_time_version: String by project
-    val guava_version: String by project
 
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1") {
         exclude("com.android.support", "support-annotations")
@@ -104,33 +103,33 @@ dependencies {
     implementation(kotlin("stdlib-jdk8", kotlin_version))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.11.0")
+    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.11.0")
     // endregion
 
     // region AndroidX
     implementation("androidx.core:core-ktx:$core_ktx_version")
+    implementation("androidx.fragment:fragment-ktx:1.1.0-beta01")
     implementation("androidx.appcompat:appcompat:$appcompat_version")
     implementation("androidx.cardview:cardview:$cardview_version")
     implementation("androidx.recyclerview:recyclerview:1.1.0-alpha06")
+    implementation("androidx.drawerlayout:drawerlayout:1.1.0-alpha02")
+    implementation("androidx.viewpager:viewpager:1.0.0")
 
     implementation("androidx.lifecycle:lifecycle-extensions:$lifecycle_version")
     kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation("com.shopify:livedata-ktx:3.0.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:$navigation_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$navigation_version")
+    //implementation("androidx.navigation:navigation-fragment-ktx:$navigation_version")
+    //implementation("androidx.navigation:navigation-ui-ktx:$navigation_version")
 
-    implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     // endregion
 
     implementation("org.jetbrains.anko:anko-common:0.10.8")
     implementation("joda-time:joda-time:$joda_time_version")
-    implementation("com.google.guava:guava:$guava_version")
 
     // region UI
     implementation("com.google.android.material:material:1.1.0-alpha07")
