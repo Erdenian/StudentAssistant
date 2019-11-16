0f67a59a03e8e8792d0492ce66527c81104a7ff9
Ilya Solovyov <mail@erdenian.ru>
2019-11-16 14:46:57 +0300

    Добавлен модуль repository

---
+++ repository/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    val compileSdkVersion: String by project
+    val targetSdkVersion: String by project
+
+    compileSdkVersion(compileSdkVersion.toInt())
+
+    defaultConfig {
+        versionCode = 1
+        versionName = "1.0"
+
+        minSdkVersion(16)
+        targetSdkVersion(targetSdkVersion.toInt())
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
+
+        productFlavors.forEach { flavor ->
+            getByName(flavor.name).java.srcDirs("src/${flavor.name}/kotlin")
+            "test${flavor.name.capitalize()}".let { getByName(it).java.srcDirs("src/$it/kotlin") }
+            "androidTest${flavor.name.capitalize()}".let { getByName(it).java.srcDirs("src/$it/kotlin") }
+        }
+    }
+}
+
+dependencies {
+    val kotlinVersion: String by project
+    val coreKtxVersion: String by project
+    val lifecycleVersion: String by project
+    val jodaTimeVersion: String by project
+
+    // region Kotlin
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    // endregion
+
+    // region AndroidX
+    api("androidx.core:core-ktx:$coreKtxVersion")
+
+    api("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion")
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    api("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    api("com.shopify:livedata-ktx:3.0.0")
+    // endregion
+
+    api("joda-time:joda-time:$jodaTimeVersion")
+}
