13883e79343155931d613d7cbd80e1e24e69fc70
Ilya Solovyov <mail@erdenian.ru>
2019-11-16 15:37:48 +0300

    Классы repository перенесены в модуль repository

---
+++ repository/src/main/kotlin/ru/erdenian/studentassistant/repository/di/Repository.kt
@@ -0,0 +1,19 @@
+package ru.erdenian.studentassistant.repository.di
+
+import android.app.Application
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+import org.kodein.di.weakReference
+import ru.erdenian.studentassistant.repository.HomeworkRepository
+import ru.erdenian.studentassistant.repository.LessonRepository
+import ru.erdenian.studentassistant.repository.SemesterRepository
+
+fun repositoryModule(application: Application) = Kodein.Module(name = "Repository") {
+    val db = databaseKodein(application)
+
+    bind() from singleton(ref = weakReference) { SemesterRepository(db.instance()) }
+    bind() from singleton(ref = weakReference) { LessonRepository(db.instance()) }
+    bind() from singleton(ref = weakReference) { HomeworkRepository(db.instance()) }
+}
