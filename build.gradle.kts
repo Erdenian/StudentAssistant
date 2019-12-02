0eba976529abf935801e23edb593a887c19c3041
Ilya Solovyov <mail@erdenian.ru>
2019-12-02 20:13:09 +0300

    Обновление detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version ("1.2.0")
+    id("io.gitlab.arturbosch.detekt") version ("1.2.1")
 }
 
 buildscript {
@@ -52,5 +52,5 @@ detekt {
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.2.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.2.1")
 }
