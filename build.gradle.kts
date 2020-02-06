b00d761658e87ee856b365c82bc338f453512c9e
Ilya Solovyov <mail@erdenian.ru>
2020-02-06 22:57:55 +0300

    Обновление Detekt до 1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version ("1.5.0")
+    id("io.gitlab.arturbosch.detekt") version ("1.5.1")
 }
 
 buildscript {
@@ -51,5 +51,5 @@ detekt {
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.5.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.5.1")
 }
