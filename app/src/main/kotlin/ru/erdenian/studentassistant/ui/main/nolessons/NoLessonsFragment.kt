70af1f98db24f714bc0b2def13584d2428d5f41a
Ilya Solovyov <mail@erdenian.ru>
2020-03-16 16:05:35 +0300

    NoLessonsFragment переделан на ViewBinding

--- app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/nolessons/NoLessonsFragment.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/nolessons/NoLessonsFragment.kt
@@ -2,19 +2,19 @@ package ru.erdenian.studentassistant.ui.main.nolessons
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import ru.erdenian.studentassistant.R
+import ru.erdenian.studentassistant.databinding.FragmentNoLessonsBinding
 import ru.erdenian.studentassistant.ui.lessoneditor.LessonEditorActivity
 import ru.erdenian.studentassistant.ui.main.MainViewModel
-import ru.erdenian.studentassistant.utils.requireViewByIdCompat
 
 class NoLessonsFragment : Fragment(R.layout.fragment_no_lessons) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel by activityViewModels<MainViewModel>()
-        view.requireViewByIdCompat<Button>(R.id.fnl_create_lesson).setOnClickListener {
+        val binding = FragmentNoLessonsBinding.bind(view)
+        binding.createLesson.setOnClickListener {
             LessonEditorActivity.start(
                 requireContext(), checkNotNull(viewModel.selectedSemester.value).id
             )
