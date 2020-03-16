4cb33e8e90df8f74046d70f3aa18d62fdea63710
Ilya Solovyov <mail@erdenian.ru>
2020-03-16 15:50:46 +0300

    Добавление ViewBinding функций в utils

--- utils/src/main/kotlin/ru/erdenian/studentassistant/utils/Fragment.kt
+++ utils/src/main/kotlin/ru/erdenian/studentassistant/utils/Fragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -45,4 +46,43 @@ private class IdDelegate<T : View>(
  * @author Ilya Solovyov
  * @since 0.3.0
  */
+@Deprecated("Использовать ViewBinding")
 fun <T : View> Fragment.id(@IdRes id: Int): ReadOnlyProperty<Any?, T> = IdDelegate(this, id)
+
+private class BindingDelegate<T : ViewBinding>(
+    private val fragment: Fragment,
+    private val binder: (View) -> T
+) : ReadOnlyProperty<Any?, T>, LifecycleObserver {
+
+    private var value: T? = null
+
+    init {
+        fragment.viewLifecycleOwnerLiveData.observe(fragment) { lifecycleOwner ->
+            lifecycleOwner.lifecycle.addObserver(this)
+        }
+    }
+
+    @Suppress("unused")
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun onDestroy() {
+        value = null
+    }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>) = checkNotNull(
+        value ?: binder(fragment.requireView()).also { value = it }
+    )
+}
+
+/**
+ * Создает делегат, поддерживающий актуальность ссылки на [ViewBinding] при его пересоздании.
+ *
+ * Если [View] в момент обращения находится в состоянии [Lifecycle.State.DESTROYED],
+ * то будет выброшено исключение.
+ *
+ * @receiver фрагмент, содержащий View
+ * @param T тип нужного ViewBinding
+ * @param binder лямбда для биндинга View
+ */
+fun <T : ViewBinding> Fragment.binding(
+    binder: (View) -> T
+): ReadOnlyProperty<Any?, T> = BindingDelegate(this, binder)
