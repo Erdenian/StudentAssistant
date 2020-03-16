35666689e942b772ddf175489fa40fdee1cd780e
Ilya Solovyov <mail@erdenian.ru>
2020-03-16 16:03:13 +0300

    NoScheduleFragment переделан на ViewBinding

--- app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/noschedule/NoScheduleFragment.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/noschedule/NoScheduleFragment.kt
@@ -2,22 +2,22 @@ package ru.erdenian.studentassistant.ui.main.noschedule
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 import ru.erdenian.studentassistant.R
+import ru.erdenian.studentassistant.databinding.FragmentNoScheduleBinding
 import ru.erdenian.studentassistant.ui.semestereditor.SemesterEditorActivity
-import ru.erdenian.studentassistant.utils.requireViewByIdCompat
 
 class NoScheduleFragment : Fragment(R.layout.fragment_no_schedule) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.requireViewByIdCompat<Button>(R.id.fns_download_schedule).setOnClickListener {
+        val binding = FragmentNoScheduleBinding.bind(view)
+
+        binding.downloadSchedule.setOnClickListener {
             requireContext().toast(R.string.nsf_download)
         }
-        view.requireViewByIdCompat<Button>(R.id.fns_create_schedule).setOnClickListener {
-            requireContext().startActivity<SemesterEditorActivity>()
+        binding.createSchedule.setOnClickListener {
+            SemesterEditorActivity.start(requireContext())
         }
     }
 }
