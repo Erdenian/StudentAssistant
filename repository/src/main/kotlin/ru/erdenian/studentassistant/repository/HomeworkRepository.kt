dd688838fb72b1474d515b823a87f7368c779ea8
Ilya Solovyov <mail@erdenian.ru>
2020-04-21 21:56:28 +0300

    Рефакторинг repository

--- repository/src/main/kotlin/ru/erdenian/studentassistant/repository/HomeworkRepository.kt
+++ repository/src/main/kotlin/ru/erdenian/studentassistant/repository/HomeworkRepository.kt
@@ -11,29 +11,20 @@ class HomeworkRepository(private val homeworkDao: HomeworkDao) : BaseRepository(
     suspend fun insert(homework: Homework) = homeworkDao.insert(homework)
 
     suspend fun get(semesterId: Long, homeworkId: Long) = homeworkDao.get(semesterId, homeworkId)
-    fun get(homework: Homework) =
-        homeworkDao.getLive(homework.semesterId, homework.id).toNullableKtx()
+    fun get(homework: Homework) = homeworkDao.getLive(homework.semesterId, homework.id).toNullableKtx()
 
     suspend fun delete(homework: Homework) = homeworkDao.delete(homework)
 
     fun get(semesterId: Long) = homeworkDao.get(semesterId).map()
     suspend fun getCount(semesterId: Long) = homeworkDao.getCount(semesterId)
 
-    fun get(semesterId: Long, subjectName: String) =
-        homeworkDao.get(semesterId, subjectName).map()
+    fun get(semesterId: Long, subjectName: String) = homeworkDao.get(semesterId, subjectName).map()
+    fun get(lesson: Lesson) = homeworkDao.get(lesson.semesterId, lesson.subjectName).map()
+    suspend fun getCount(semesterId: Long, subjectName: String) = homeworkDao.getCount(semesterId, subjectName)
 
-    fun get(lesson: Lesson) =
-        homeworkDao.get(lesson.semesterId, lesson.subjectName).map()
-
-    suspend fun getCount(semesterId: Long, subjectName: String) =
-        homeworkDao.getCount(semesterId, subjectName)
-
-    suspend fun hasHomeworks(semesterId: Long, subjectName: String) =
-        homeworkDao.hasHomeworks(semesterId, subjectName)
+    suspend fun hasHomeworks(semesterId: Long, subjectName: String) = homeworkDao.hasHomeworks(semesterId, subjectName)
 
     fun getActual(semesterId: Long) = homeworkDao.getActual(semesterId).map()
+    fun getActual(lesson: Lesson) = homeworkDao.getActual(lesson.semesterId, lesson.subjectName).map()
     fun getPast(semesterId: Long) = homeworkDao.getPast(semesterId).map()
-
-    fun getActual(lesson: Lesson) =
-        homeworkDao.getActual(lesson.semesterId, lesson.subjectName).map()
 }
--- repository/src/main/kotlin/ru/erdenian/studentassistant/repository/LessonRepository.kt
+++ repository/src/main/kotlin/ru/erdenian/studentassistant/repository/LessonRepository.kt
@@ -10,6 +10,7 @@ import ru.erdenian.studentassistant.entity.Semester
 class LessonRepository(private val lessonDao: LessonDao) : BaseRepository() {
 
     suspend fun insert(lesson: Lesson) = lessonDao.insert(lesson)
+
     suspend fun get(semesterId: Long, lessonId: Long) = lessonDao.get(semesterId, lessonId)
     fun get(lesson: Lesson) = lessonDao.getLive(lesson.semesterId, lesson.id).toNullableKtx()
     fun hasLessons(semesterId: Long) = lessonDao.hasLessons(semesterId)
@@ -19,8 +20,7 @@ class LessonRepository(private val lessonDao: LessonDao) : BaseRepository() {
     fun get(semester: Semester, day: LocalDate) = lessonDao.get(semester, day).map()
     fun get(semesterId: Long, weekday: Int) = lessonDao.get(semesterId, weekday).map()
     suspend fun getCount(semesterId: Long) = lessonDao.getCount(semesterId)
-    suspend fun getCount(semesterId: Long, subjectName: String) =
-        lessonDao.getCount(semesterId, subjectName)
+    suspend fun getCount(semesterId: Long, subjectName: String) = lessonDao.getCount(semesterId, subjectName)
 
     fun getSubjects(semesterId: Long) = lessonDao.getSubjects(semesterId).map()
     suspend fun renameSubject(semesterId: Long, oldName: String, newName: String) =
@@ -30,6 +30,5 @@ class LessonRepository(private val lessonDao: LessonDao) : BaseRepository() {
     fun getTeachers(semesterId: Long) = lessonDao.getTeachers(semesterId).map()
     fun getClassrooms(semesterId: Long) = lessonDao.getClassrooms(semesterId).map()
     suspend fun getLessonLength(semesterId: Long) = lessonDao.getLessonLength(semesterId)
-    suspend fun getNextStartTime(semesterId: Long, weekday: Int) =
-        lessonDao.getNextStartTime(semesterId, weekday)
+    suspend fun getNextStartTime(semesterId: Long, weekday: Int) = lessonDao.getNextStartTime(semesterId, weekday)
 }
