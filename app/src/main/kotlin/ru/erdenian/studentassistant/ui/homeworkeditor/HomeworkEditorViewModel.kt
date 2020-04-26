dad71d28eaf1ac2eecb34e588a76a039d78b783e
Ilya Solovyov <mail@erdenian.ru>
2020-04-26 20:19:44 +0300

    Исправление HomeworkEditorActivity

--- app/src/main/kotlin/ru/erdenian/studentassistant/ui/homeworkeditor/HomeworkEditorViewModel.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/homeworkeditor/HomeworkEditorViewModel.kt
@@ -2,13 +2,14 @@ package ru.erdenian.studentassistant.ui.homeworkeditor
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.map
 import androidx.lifecycle.switchMap
-import com.shopify.livedataktx.LiveDataKtx
-import com.shopify.livedataktx.MediatorLiveDataKtx
-import com.shopify.livedataktx.MutableLiveDataKtx
-import com.shopify.livedataktx.toKtx
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import org.joda.time.LocalDate
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
@@ -17,7 +18,7 @@ import ru.erdenian.studentassistant.entity.Homework
 import ru.erdenian.studentassistant.repository.HomeworkRepository
 import ru.erdenian.studentassistant.repository.LessonRepository
 import ru.erdenian.studentassistant.repository.SemesterRepository
-import ru.erdenian.studentassistant.utils.asLiveData
+import ru.erdenian.studentassistant.utils.setIfEmpty
 
 class HomeworkEditorViewModel(
     application: Application
@@ -32,16 +33,17 @@ class HomeworkEditorViewModel(
         EMPTY_DESCRIPTION
     }
 
-    private val semesterId = MutableLiveDataKtx<Long>()
+    private val semesterIdPrivate = MutableLiveData<Long>()
+    val semesterId: LiveData<Long> get() = semesterIdPrivate
     private var homework: Homework? = null
 
     fun init(semesterId: Long, subjectName: String?) {
-        this.semesterId.value = semesterId
+        this.semesterIdPrivate.setIfEmpty(semesterId)
         subjectName?.let { this.subjectName.value = it }
     }
 
     fun init(semesterId: Long, homework: Homework?) {
-        this.semesterId.value = semesterId
+        this.semesterIdPrivate.setIfEmpty(semesterId)
 
         this.homework = homework?.also { h ->
             subjectName.value = h.subjectName
@@ -50,44 +52,54 @@ class HomeworkEditorViewModel(
         }
     }
 
-    val existingSubjects = semesterId.asLiveData
-        .switchMap { lessonRepository.getSubjects(it) }.toKtx()
-    val semesterLastDay = semesterId.asLiveData
-        .switchMap { semesterRepository.get(it) }
-        .map { checkNotNull(it).lastDay }.toKtx()
+    val existingSubjects = semesterId.switchMap { lessonRepository.getSubjects(it) }
+    val semesterLastDay = semesterId
+        .switchMap { semesterRepository.getLiveData(it) }
+        .map { checkNotNull(it).lastDay }
 
-    val subjectName: MutableLiveDataKtx<String> = MediatorLiveDataKtx<String>().apply {
-        addSource(existingSubjects, Observer { if (safeValue !in it) value = it.firstOrNull() ?: "" })
+    val subjectName: MutableLiveData<String> = MediatorLiveData<String>().apply {
+        addSource(existingSubjects) { if (value !in it) value = it.firstOrNull() ?: "" }
     }
-    val description = MutableLiveDataKtx<String>().apply { value = "" }
-    val deadline = MutableLiveDataKtx<LocalDate>().apply { value = LocalDate.now() }
+    val description = MutableLiveData("")
+    val deadline = MutableLiveData(LocalDate.now())
 
-    val error: LiveDataKtx<Error?> = MediatorLiveDataKtx<Error?>().apply {
-        val onChanged = Observer<Any?> {
-            value = if (description.safeValue?.isBlank() != false) Error.EMPTY_DESCRIPTION else null
+    val error: LiveData<Error?> = MediatorLiveData<Error?>().apply {
+        val observer = Observer<Any?> {
+            value = if (description.value?.isBlank() != false) Error.EMPTY_DESCRIPTION else null
         }
 
-        addSource(description, onChanged)
+        addSource(description, observer)
     }
 
-    val lessonExists get() = subjectName.value in existingSubjects.value
+    val lessonExists get() = checkNotNull(subjectName.value) in checkNotNull(existingSubjects.value)
 
-    suspend fun save(): Long {
+    private val donePrivate = MutableLiveData(false)
+    val done: LiveData<Boolean> get() = donePrivate
+
+    fun save() {
         check(error.value == null)
 
-        val oldHomework = homework
-        val newHomework = Homework(
-            subjectName.value,
-            description.value,
-            deadline.value,
-            semesterId.value
-        ).run { oldHomework?.let { copy(id = it.id) } ?: this }
-
-        homeworkRepository.insert(newHomework)
-        return newHomework.id
+        viewModelScope.launch {
+            homework?.let { homework ->
+                homeworkRepository.update(
+                    homework.id,
+                    checkNotNull(subjectName.value),
+                    checkNotNull(description.value),
+                    checkNotNull(deadline.value),
+                    checkNotNull(semesterId.value)
+                )
+            } ?: run {
+                homeworkRepository.insert(
+                    checkNotNull(subjectName.value),
+                    checkNotNull(description.value),
+                    checkNotNull(deadline.value),
+                    checkNotNull(semesterId.value)
+                )
+            }
+        }
     }
 
-    suspend fun delete() {
-        homework?.let { homeworkRepository.delete(it) }
+    fun delete() {
+        viewModelScope.launch { homeworkRepository.delete(checkNotNull(homework).id) }
     }
 }
