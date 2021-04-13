e47b298c2d392e9f8c4ae73da50ddd6e615e11cc
Ilya Solovyov <mail@erdenian.ru>
2021-04-14 02:11:21 +0300

    Рефакторинг

--- app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/settings/TimePreference.kt
+++ app/src/main/kotlin/ru/erdenian/studentassistant/ui/main/settings/TimePreference.kt
@@ -1,10 +1,12 @@
 package ru.erdenian.studentassistant.ui.main.settings
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
-import androidx.core.content.withStyledAttributes
 import androidx.preference.DialogPreference
+import androidx.preference.Preference.SummaryProvider
 import org.joda.time.LocalTime
+import org.joda.time.format.DateTimeFormat
 import ru.erdenian.studentassistant.R
 
 class TimePreference @JvmOverloads constructor(
@@ -14,26 +16,26 @@ class TimePreference @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : DialogPreference(context, attrs, defStyleAttr, defStyleRes) {
 
-    private var defaultTimeMillis = DEFAULT_TIME_MILLIS
-
     init {
-        context.withStyledAttributes(attrs, R.styleable.Preference, defStyleAttr) {
-            defaultTimeMillis = getInt(R.styleable.Preference_defaultValue, DEFAULT_TIME_MILLIS)
+        val timeFormatter = DateTimeFormat.forPattern("HH:mm")
+        summaryProvider = SummaryProvider<TimePreference> { preference -> preference.time.toString(timeFormatter) }
+    }
+
+    var time = getPersistedInt(DEFAULT_TIME_MILLIS).toLocalTime()
+        set(value) {
+            field = value
+            persistInt(value.toInt())
+            notifyChanged()
         }
-        summary = getPersistedTime().toString("HH:mm")
-    }
 
-    fun getPersistedTime() = getPersistedInt(defaultTimeMillis).let(LocalTime.MIDNIGHT::plusMillis)
-
-    fun persistTime(time: LocalTime) {
-        persistInt(time.millisOfDay)
-        notifyChanged()
-    }
+    override fun onGetDefaultValue(a: TypedArray, index: Int) = a.getInteger(index, DEFAULT_TIME_MILLIS).toLocalTime()
 
     override fun onSetInitialValue(defaultValue: Any?) {
-        super.onSetInitialValue(defaultValue)
-        summary = getPersistedTime().toString("HH:mm")
+        time = (defaultValue as LocalTime?) ?: return
     }
+
+    private fun LocalTime.toInt() = millisOfDay
+    private fun Int.toLocalTime(): LocalTime = LocalTime.MIDNIGHT.plusMillis(this)
 
     companion object {
         private const val DEFAULT_TIME_MILLIS = 0
