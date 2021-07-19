2880ffee6b0fdcfdf57da927d0ab5f174203684a
Ilya Solovyov <mail@erdenian.ru>
2021-07-19 16:19:45 +0300

    WeeksSelector переписан на Compose

--- uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/views/WeeksSelector.kt
+++ uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/views/WeeksSelector.kt
@@ -1,49 +1,51 @@
 package ru.erdenian.studentassistant.uikit.views
 
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.CompoundButton
-import android.widget.HorizontalScrollView
-import android.widget.LinearLayout
-import android.widget.Spinner
-import androidx.core.content.getSystemService
-import androidx.core.view.children
-import androidx.core.view.forEach
-import androidx.core.view.setPadding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import ru.erdenian.studentassistant.uikit.R
-import ru.erdenian.studentassistant.uikit.databinding.WeeksSelectorBinding
-import ru.erdenian.studentassistant.utils.setViewCount
 
 /**
  * View для выбора недель для повторения пары.
- *
- * Состоит из спиннера с предустановленными вариантами и чекбоксов для самостоятельного
- * выбора недель, если ни один из предустановленных вариантов не подходит.
- *
- * @see Spinner
- * @see CheckBoxWithText
- *
- * @version 1.0.0
- * @author Ilya Solovyov
- * @since 0.2.6
  */
-class WeeksSelector @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
-
-    private val binding = WeeksSelectorBinding.inflate(requireNotNull(context.getSystemService()), this)
-
-    /**
-     * Список предустановленных вариантов.
-     *
-     * @since 0.2.6
-     */
-    private val weeksVariants = listOf(
+@Composable
+fun WeeksSelector(
+    weeks: List<Boolean>,
+    onWeeksChange: (weeks: List<Boolean>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val repeatVariants = stringArrayResource(R.array.repeat_variants).toList()
+    val weeksVariants = listOf(
         listOf(true),
         listOf(true, false),
         listOf(false, true),
@@ -53,130 +55,276 @@ class WeeksSelector @JvmOverloads constructor(
         listOf(false, false, false, true)
     )
 
-    /**
-     * Возвращает номер выбранного варианта из предустановок.
-     *
-     * @param weeks список недель
-     * @return номер выбранного варианта
-     * @since 0.2.6
-     */
-    private fun getWeeksVariantIndex(weeks: List<Boolean>) =
-        weeksVariants.indexOf(weeks).takeIf { it >= 0 } ?: weeksVariants.size
+    var selectedRepeatVariantIndex by rememberSaveable {
+        val index = weeksVariants.indexOf(weeks).takeIf { it >= 0 } ?: weeksVariants.size
+        mutableStateOf(index)
+    }
+    var repeatVariantsExpanded by remember { mutableStateOf(false) }
 
-    private val onCheckedChangeListener = CompoundButton.OnCheckedChangeListener { _, _ ->
-        onWeeksChangeListener?.invoke(weeks)
+    val isCustomEnabled = (selectedRepeatVariantIndex >= weeksVariants.size)
+
+    WeeksSelectorView(
+        repeatVariants = repeatVariants,
+        selectedRepeatVariantIndex = selectedRepeatVariantIndex,
+        repeatVariantsExpanded = repeatVariantsExpanded,
+        onSelectedRepeatVariantClick = { repeatVariantsExpanded = true },
+        onRepeatVariantsDismissRequest = { repeatVariantsExpanded = false },
+        onRepeatVariantClick = { index ->
+            selectedRepeatVariantIndex = index
+            onWeeksChange(weeksVariants.getOrElse(index) { weeks })
+            repeatVariantsExpanded = false
+        },
+        weeks = weeks,
+        onWeekClick = { index, checked ->
+            val mutableWeeks = weeks.toBooleanArray()
+            mutableWeeks[index] = checked
+            onWeeksChange(mutableWeeks.toList())
+        },
+        onMinusClick = { onWeeksChange(weeks.dropLast(1)) },
+        onPlusClick = { onWeeksChange(weeks + false) },
+        isMinusEnabled = (weeks.size > 1) && isCustomEnabled,
+        isPlusEnabled = isCustomEnabled,
+        isCustomEnabled = isCustomEnabled,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun WeeksSelectorView(
+    repeatVariants: List<String>,
+    selectedRepeatVariantIndex: Int,
+    repeatVariantsExpanded: Boolean,
+    onSelectedRepeatVariantClick: () -> Unit,
+    onRepeatVariantsDismissRequest: () -> Unit,
+    onRepeatVariantClick: (index: Int) -> Unit,
+    weeks: List<Boolean>,
+    onWeekClick: (index: Int, checked: Boolean) -> Unit,
+    onMinusClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    isMinusEnabled: Boolean,
+    isPlusEnabled: Boolean,
+    isCustomEnabled: Boolean,
+    modifier: Modifier = Modifier
+) = ConstraintLayout(modifier = modifier) {
+    val (
+        repeatTitle, selectedRepeatVariant, repeatDropdownMenu,
+        minusButton, minusDivider,
+        checkboxesScroll,
+        plusDivider, plusButton
+    ) = createRefs()
+
+    val barrier = createBottomBarrier(repeatTitle)
+    createHorizontalChain(
+        repeatTitle,
+        selectedRepeatVariant,
+        chainStyle = ChainStyle.Packed(0.0f)
+    )
+
+    Text(
+        text = stringResource(R.string.ws_variants_title),
+        modifier = Modifier
+            .constrainAs(repeatTitle) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(selectedRepeatVariant.start)
+                bottom.linkTo(barrier)
+            }
+            .padding(
+                start = dimensionResource(R.dimen.activity_horizontal_margin),
+                end = 8.dp
+            )
+    )
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .constrainAs(selectedRepeatVariant) {
+                top.linkTo(parent.top)
+                start.linkTo(repeatTitle.end)
+                end.linkTo(parent.end)
+                bottom.linkTo(barrier)
+            }
+            .padding(end = dimensionResource(R.dimen.activity_horizontal_margin))
+            .clickable(onClick = onSelectedRepeatVariantClick)
+    ) {
+        Text(
+            text = repeatVariants[selectedRepeatVariantIndex],
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+
+        Icon(
+            painter = painterResource(R.drawable.ic_plus),
+            contentDescription = null
+        )
     }
 
-    private val checkboxPadding = context.resources.getDimensionPixelSize(R.dimen.weeks_selector_checkbox_padding)
-    private val creator: ViewGroup.(position: Int) -> CheckBoxWithText = { position ->
-        CheckBoxWithText(context).apply {
-            text = (position + 1).toString()
-            setPadding(checkboxPadding)
-            setOnCheckedChangeListener(onCheckedChangeListener)
-        }
-    }
-
-    /**
-     * Список недель на текущий момент.
-     *
-     * Список недель - список boolean значений, где i-е значение показывает была ли выбрана
-     * i-я неделя. Если список недель состоит из нескольких повторяющихся последовательностей,
-     * то вернет только одну из них. Например, в случае списка
-     * `{ true, false, true, true, false, true }` вернет `{ true, false, true }`.
-     *
-     * @since 0.2.6
-     */
-    var weeks: List<Boolean>
-        get() {
-            val weeks = binding.weeksParent.children.map { v ->
-                @Suppress("UnsafeCast")
-                (v as CheckBoxWithText).isChecked
-            }.toList()
-            cycleLengthLoop@ for (cycleLength in 1..(weeks.size / 2)) {
-                if (weeks.size % cycleLength != 0) continue
-                for (offset in cycleLength until weeks.size step cycleLength) {
-                    for (position in 0 until cycleLength) {
-                        if (weeks[position] != weeks[offset + position]) continue@cycleLengthLoop
-                    }
-                }
-                return weeks.take(cycleLength)
+    DropdownMenu(
+        expanded = repeatVariantsExpanded,
+        onDismissRequest = onRepeatVariantsDismissRequest,
+        modifier = Modifier
+            .constrainAs(repeatDropdownMenu) {
+                top.linkTo(selectedRepeatVariant.top)
+                start.linkTo(selectedRepeatVariant.start)
+                width = Dimension.wrapContent
             }
-            return weeks
-        }
-        set(weeks) {
-            val selection = getWeeksVariantIndex(weeks)
-
-            binding.variants.apply {
-                val listener = onItemSelectedListener
-                onItemSelectedListener = null
-                // При использовании setSelection(int) вызывается обработчик. Хз почему.
-                setSelection(selection, true)
-                onItemSelectedListener = listener
-            }
-
-            binding.weeksParent.setViewCount(weeks.size, creator) { isChecked = weeks[it] }
-
-            setCustomEnabled(selection == weeksVariants.size)
-        }
-
-    var onWeeksChangeListener: ((weeks: List<Boolean>) -> Unit)? = null
-
-    private val enabledTint = binding.remove.imageTintList
-    private val disabledTint = binding.add.imageTintList
-
-    init {
-        orientation = VERTICAL
-
-        check(binding.variants.adapter.count == weeksVariants.size + 1) {
-            "Несоответствие вариантов выбора и количества предустановок"
-        }
-
-        weeks = weeksVariants.first()
-
-        binding.variants.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View?,
-                position: Int,
-                id: Long
+    ) {
+        repeatVariants.forEachIndexed { index, variant ->
+            DropdownMenuItem(
+                onClick = { onRepeatVariantClick(index) }
             ) {
-                weeks = weeksVariants.getOrNull(position) ?: return setCustomEnabled(true)
+                Text(text = variant)
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>) = Unit
-        }
-
-        binding.remove.setOnClickListener {
-            binding.weeksParent.setViewCount(binding.weeksParent.childCount - 1, creator)
-            setCustomEnabled(true)
-            onWeeksChangeListener?.invoke(weeks)
-        }
-        binding.add.setOnClickListener {
-            binding.weeksParent.setViewCount(binding.weeksParent.childCount + 1, creator)
-            setCustomEnabled(true)
-            binding.scroll.post { binding.scroll.fullScroll(HorizontalScrollView.FOCUS_RIGHT) }
-            onWeeksChangeListener?.invoke(weeks)
-        }
-        binding.weeksParent.setViewCount(binding.weeksParent.childCount, creator) {
-            setOnCheckedChangeListener(onCheckedChangeListener)
         }
     }
 
-    /**
-     * Активирует или деактивирует элементы интерфейса, предназначенные для выбора недель вручную.
-     *
-     * @param enabled true для активации, false для деактивации
-     * @since 0.2.6
-     */
-    private fun setCustomEnabled(enabled: Boolean) {
-        binding.remove.apply {
-            isEnabled = (enabled && binding.weeksParent.childCount > 1)
-            imageTintList = (if (isEnabled) enabledTint else disabledTint)
+    IconButton(
+        onClick = onMinusClick,
+        modifier = Modifier
+            .constrainAs(minusButton) {
+                top.linkTo(checkboxesScroll.top)
+                start.linkTo(parent.start)
+                end.linkTo(minusDivider.start)
+                bottom.linkTo(checkboxesScroll.bottom)
+            },
+        enabled = isMinusEnabled
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_minus),
+            contentDescription = null
+        )
+    }
+
+    Divider(modifier = Modifier
+        .padding(vertical = 8.dp)
+        .width(1.dp)
+        .constrainAs(minusDivider) {
+            start.linkTo(minusButton.end)
+            top.linkTo(checkboxesScroll.top)
+            end.linkTo(checkboxesScroll.start)
+            bottom.linkTo(checkboxesScroll.bottom)
+            height = Dimension.fillToConstraints
         }
-        binding.add.apply {
-            isEnabled = enabled
-            imageTintList = (if (isEnabled) enabledTint else disabledTint)
+    )
+
+    LazyRow(
+        modifier = Modifier
+            .constrainAs(checkboxesScroll) {
+                top.linkTo(barrier)
+                start.linkTo(minusDivider.end)
+                end.linkTo(plusDivider.start)
+                bottom.linkTo(parent.bottom)
+                width = Dimension.fillToConstraints
+            }
+    ) {
+        itemsIndexed(weeks) { index, checked ->
+            CheckBoxWithText(
+                checked = checked,
+                text = (index + 1).toString(),
+                onCheckedChange = { onWeekClick(index, it) },
+                enabled = isCustomEnabled
+            )
         }
-        binding.weeksParent.forEach { it.isEnabled = enabled }
+    }
+
+    Divider(modifier = Modifier
+        .padding(vertical = 8.dp)
+        .width(1.dp)
+        .constrainAs(plusDivider) {
+            top.linkTo(checkboxesScroll.top)
+            start.linkTo(checkboxesScroll.end)
+            bottom.linkTo(checkboxesScroll.bottom)
+            end.linkTo(plusButton.start)
+            height = Dimension.fillToConstraints
+        }
+    )
+
+    IconButton(
+        onClick = onPlusClick,
+        modifier = Modifier
+            .constrainAs(plusButton) {
+                top.linkTo(checkboxesScroll.top)
+                start.linkTo(plusDivider.end)
+                end.linkTo(parent.end)
+                bottom.linkTo(checkboxesScroll.bottom)
+            },
+        enabled = isPlusEnabled
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_plus),
+            contentDescription = null
+        )
     }
 }
+
+@Preview
+@Composable
+private fun WeeksSelectorPreview() = WeeksSelectorView(
+    repeatVariants = listOf("По чётным", "По нечётным", "Своё"),
+    selectedRepeatVariantIndex = 2,
+    repeatVariantsExpanded = false,
+    onSelectedRepeatVariantClick = {},
+    onRepeatVariantsDismissRequest = {},
+    onRepeatVariantClick = {},
+    weeks = listOf(true, false, true),
+    onWeekClick = { _, _ -> },
+    onMinusClick = {},
+    onPlusClick = {},
+    isMinusEnabled = true,
+    isPlusEnabled = true,
+    isCustomEnabled = true,
+    modifier = Modifier.fillMaxWidth()
+)
+
+@Preview
+@Composable
+private fun WeeksSelectorPreviewLongRepeatVariant() = WeeksSelectorView(
+    repeatVariants = listOf("По чётным чётным чётным чётным чётным чётным чётным"),
+    selectedRepeatVariantIndex = 0,
+    repeatVariantsExpanded = false,
+    onSelectedRepeatVariantClick = {},
+    onRepeatVariantsDismissRequest = {},
+    onRepeatVariantClick = {},
+    weeks = listOf(true, false, true),
+    onWeekClick = { _, _ -> },
+    onMinusClick = {},
+    onPlusClick = {},
+    isMinusEnabled = true,
+    isPlusEnabled = true,
+    isCustomEnabled = true,
+    modifier = Modifier.fillMaxWidth()
+)
+
+@Composable
+private fun CheckBoxWithText(
+    checked: Boolean,
+    text: String,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) = Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+        .padding(4.dp)
+        .clickable { if (enabled) onCheckedChange?.invoke(!checked) }
+        .then(modifier)
+) {
+    Checkbox(checked = checked, onCheckedChange = null, enabled = enabled)
+    Text(text = text, maxLines = 1)
+}
+
+@Preview(name = "Short text")
+@Composable
+private fun CheckBoxWithTextPreviewShort() = CheckBoxWithText(true, "1", null)
+
+@Preview(name = "Medium text")
+@Composable
+private fun CheckBoxWithTextPreviewMedium() = CheckBoxWithText(true, "Text", null)
+
+@Preview(name = "Long text")
+@Composable
+private fun CheckBoxWithTextPreviewLong() = CheckBoxWithText(true, "Long text", null)
+
+@Preview(name = "Disabled")
+@Composable
+private fun CheckBoxWithTextPreviewDisabled() = CheckBoxWithText(true, "Disabled", null, enabled = false)
