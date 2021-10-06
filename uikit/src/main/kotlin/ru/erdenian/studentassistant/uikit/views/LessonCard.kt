3ad1cd22ae39dcbcbae9e220e4fd4b595caf0f34
Ilya Solovyov <mail@erdenian.ru>
2021-10-06 16:55:32 +0300

    Add TextCompat composable

--- uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/views/LessonCard.kt
+++ uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/views/LessonCard.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
@@ -22,6 +20,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,15 +74,16 @@ fun LessonCard(
                     style = MaterialTheme.typography.body1
                 )
 
-                Spacer(modifier = Modifier.width(32.dp))
-                Spacer(modifier = Modifier.weight(1.0f))
-
                 if (classrooms.isNotEmpty()) {
-                    Text(
+                    TextCompat(
                         text = classrooms.joinToString(),
+                        textAlign = TextAlign.End,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
-                        style = MaterialTheme.typography.body1
+                        style = MaterialTheme.typography.body1,
+                        modifier = Modifier
+                            .weight(1.0f)
+                            .padding(start = 32.dp)
                     )
                     Icon(
                         imageVector = AppIcons.LocationOn,
