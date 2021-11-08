b6651e826429b09d08b48d003f8021a047a9b96c
Ilya Solovyov <mail@erdenian.ru>
2021-11-08 21:14:53 +0300

    Refactor Lint settings

--- common/utils/src/main/kotlin/ru/erdenian/studentassistant/utils/Anko.kt
+++ common/utils/src/main/kotlin/ru/erdenian/studentassistant/utils/Anko.kt
@@ -2,7 +2,5 @@ package ru.erdenian.studentassistant.utils
 
 import android.content.Context
 import android.widget.Toast
-import androidx.annotation.StringRes
 
-fun Context.toast(@StringRes resId: Int, length: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, resId, length).show()
 fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, text, length).show()
