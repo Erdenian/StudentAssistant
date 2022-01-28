ad3b13186be9740ac4f813051aa1f59c0747d5c4
Ilya Solovyov <mail@erdenian.ru>
2022-01-28 22:00:52 +0300

    Move Context.toast to Context.kt

--- common/utils/src/main/kotlin/ru/erdenian/studentassistant/utils/Anko.kt
+++
@@ -1,6 +0,0 @@
-package ru.erdenian.studentassistant.utils
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, text, length).show()
