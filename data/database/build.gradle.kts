3e91815ab5009026ef6dc190fdc13696bef25604
Ilya Solovyov <mail@erdenian.ru>
2022-02-10 15:27:36 +0300

    Refactor build scripts

--- data/database/build.gradle.kts
+++ data/database/build.gradle.kts
@@ -6,54 +6,13 @@ plugins {
 }
 
 android {
-    val minSdkVersion: String by project
-    val compileSdkVersion: String by project
-    val targetSdkVersion: String by project
-
-    compileSdk = compileSdkVersion.toInt()
-
     defaultConfig {
-        minSdk = minSdkVersion.toInt()
-        targetSdk = targetSdkVersion.toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        consumerProguardFiles("consumer-rules.pro")
-
         javaCompileOptions {
             annotationProcessorOptions {
                 arguments["room.schemaLocation"] = "$projectDir/schemas"
                 arguments["room.incremental"] = "true"
                 arguments["room.expandProjection"] = "true"
             }
-        }
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
-        getByName("test").java.srcDirs("src/test/kotlin")
-        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
-
-        productFlavors.forEach { flavor ->
-            getByName(flavor.name).java.srcDirs("src/${flavor.name}/kotlin")
-            "test${flavor.name.capitalize()}".let { getByName(it).java.srcDirs("src/$it/kotlin") }
-            "androidTest${flavor.name.capitalize()}".let { getByName(it).java.srcDirs("src/$it/kotlin") }
         }
     }
 }
