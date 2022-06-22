b7a2cb6d47808ef3684a7a97db24dfd74a090c4b
Ilya Solovyov <mail@erdenian.com>
2022-06-22 13:19:01 +0300

    Refactoring

--- core/style/build.gradle.kts
+++ core/style/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "com.erdenian.studentassistant.style"
+
     buildFeatures.compose = true
 }
 
--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 android {
     namespace = "com.erdenian.studentassistant.settings"
+
     buildFeatures.compose = true
 }
 
