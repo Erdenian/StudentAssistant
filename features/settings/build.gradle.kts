81386691206e41606de352fb4a552df2fb1f9794
Ilya Solovyov <mail@erdenian.com>
2023-04-14 14:30:38 +0300

    Update Android Gradle Plugin to 8.0.0

--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -1,4 +1,3 @@
-@Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
