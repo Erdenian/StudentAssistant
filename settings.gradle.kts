dae9cd4645b2f9814c92764f0d04612b41e954e0
Ilya Solovyov <mail@erdenian.com>
2023-12-08 23:17:30 +0300

    Update Gradle and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -43,13 +43,13 @@ dependencyResolutionManagement {
 
         create("libs") {
             // region Plugins
-            version("plugins.android", "8.1.1")
+            version("plugins.android", "8.2.0")
             plugin("android.application", "com.android.application")
                 .versionRef("plugins.android")
             plugin("android.library", "com.android.library")
                 .versionRef("plugins.android")
 
-            version("plugins.kotlin", "1.9.10")
+            version("plugins.kotlin", "1.9.21")
             plugin("kotlin.android", "org.jetbrains.kotlin.android")
                 .versionRef("plugins.kotlin")
             plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm")
@@ -57,9 +57,9 @@ dependencyResolutionManagement {
             plugin("kotlin.parcelize", "org.jetbrains.kotlin.plugin.parcelize")
                 .versionRef("plugins.kotlin")
             plugin("kotlin.ksp", "com.google.devtools.ksp")
-                .version("1.9.10-1.0.13")
+                .version("1.9.21-1.0.15")
 
-            version("plugins.detekt", "1.23.1")
+            version("plugins.detekt", "1.23.4")
             plugin("detekt", "io.gitlab.arturbosch.detekt")
                 .versionRef("plugins.detekt")
             library("detekt.formatting", "io.gitlab.arturbosch.detekt", "detekt-formatting")
@@ -68,7 +68,7 @@ dependencyResolutionManagement {
             version("plugins.jacoco", "0.8.9")
 
             plugin("tripletPlay", "com.github.triplet.play")
-                .version("3.8.4")
+                .version("3.8.6")
 
             plugin("shrinkometer", "ru.erdenian.shrinkometer")
                 .version("0.3.1")
@@ -84,7 +84,7 @@ dependencyResolutionManagement {
             library("test.junit", "junit", "junit")
                 .version("4.13.2")
             library("test.mockk", "io.mockk", "mockk")
-                .version("1.13.7")
+                .version("1.13.8")
 
             library("test.androidx.junitKtx", "androidx.test.ext", "junit-ktx")
                 .version("1.1.5")
@@ -120,14 +120,14 @@ dependencyResolutionManagement {
 
             // region Android Tools
             library("androidTools.desugarJdkLibs", "com.android.tools", "desugar_jdk_libs")
-                .version("2.0.3")
+                .version("2.0.4")
             // endregion
 
             // region Compose
-            version("androidx.compose.compiler", "1.5.3")
+            version("androidx.compose.compiler", "1.5.6")
 
             library("androidx.compose.bom", "androidx.compose", "compose-bom")
-                .version("2023.09.00")
+                .version("2023.10.01")
 
             library("androidx.compose.ui", "androidx.compose.ui", "ui")
                 .withoutVersion()
@@ -161,7 +161,7 @@ dependencyResolutionManagement {
             library("androidx.lifecycle.viewmodel", "androidx.lifecycle", "lifecycle-viewmodel-ktx")
                 .version("2.6.2")
 
-            version("androidx.room", "2.5.2")
+            version("androidx.room", "2.6.1")
             library("androidx.room.compiler", "androidx.room", "room-compiler")
                 .versionRef("androidx.room")
             library("androidx.room", "androidx.room", "room-ktx")
@@ -171,17 +171,17 @@ dependencyResolutionManagement {
                 .version("1.6.1")
 
             library("androidx.activity", "androidx.activity", "activity-compose")
-                .version("1.7.2")
+                .version("1.8.1")
 
             library("androidx.navigation", "androidx.navigation", "navigation-compose")
-                .version("2.7.2")
+                .version("2.7.5")
 
             library("androidx.core.splashscreen", "androidx.core", "core-splashscreen")
                 .version("1.0.1")
             // endregion
 
             // region Core
-            version("core.dagger", "2.48")
+            version("core.dagger", "2.49")
             library("core.dagger.compiler", "com.google.dagger", "dagger-compiler")
                 .versionRef("core.dagger")
             library("core.dagger", "com.google.dagger", "dagger")
