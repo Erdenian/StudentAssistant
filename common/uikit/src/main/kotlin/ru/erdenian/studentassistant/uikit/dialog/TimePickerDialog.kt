ea236a51e7fbe93892aa7839352c5db00d82e72f
Ilya Solovyov <mail@erdenian.com>
2025-11-29 22:54:41 +0300

    Use new TimePickerDialog

--- common/uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/dialog/TimePickerDialog.kt
+++ common/uikit/src/main/kotlin/ru/erdenian/studentassistant/uikit/dialog/TimePickerDialog.kt
@@ -1,45 +1,26 @@
 package ru.erdenian.studentassistant.uikit.dialog
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.filled.Keyboard
 import androidx.compose.material.icons.filled.Schedule
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimeInput
 import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerDialog
 import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import java.time.LocalTime
 import ru.erdenian.studentassistant.strings.RS
 import ru.erdenian.studentassistant.style.AppIcons
@@ -54,9 +35,7 @@ fun TimePickerDialog(
     var mode: DisplayMode by remember { mutableStateOf(DisplayMode.Picker) }
     val state: TimePickerState = rememberTimePickerState(initialTime.hour, initialTime.minute)
 
-    // TimePicker does not provide a default TimePickerDialog, so we use our own PickerDialog:
-    // https://issuetracker.google.com/issues/288311426
-    PickerDialog(
+    TimePickerDialog(
         modifier = modifier,
         onDismissRequest = onDismiss,
         title = {
@@ -68,9 +47,10 @@ fun TimePickerDialog(
                         else -> RS.tpd_enter_time
                     },
                 ),
+                modifier = Modifier.padding(bottom = 24.dp),
             )
         },
-        buttons = {
+        modeToggleButton = {
             IconButton(
                 modifier = modifier,
                 onClick = when (mode) {
@@ -88,15 +68,15 @@ fun TimePickerDialog(
                     contentDescription = "",
                 )
             }
-
-            Spacer(Modifier.weight(1.0f))
-
+        },
+        dismissButton = {
             TextButton(
                 onClick = onDismiss,
             ) {
                 Text(text = stringResource(android.R.string.cancel))
             }
-
+        },
+        confirmButton = {
             TextButton(
                 onClick = { onConfirm(LocalTime.of(state.hour, state.minute)) },
             ) {
@@ -108,57 +88,6 @@ fun TimePickerDialog(
         when (mode) {
             DisplayMode.Picker -> TimePicker(state = state, modifier = contentModifier)
             DisplayMode.Input -> TimeInput(state = state, modifier = contentModifier)
-        }
-    }
-}
-
-@Composable
-private fun PickerDialog(
-    onDismissRequest: () -> Unit,
-    title: @Composable () -> Unit,
-    buttons: @Composable RowScope.() -> Unit,
-    modifier: Modifier = Modifier,
-    content: @Composable ColumnScope.() -> Unit,
-) = BasicAlertDialog(
-    onDismissRequest = onDismissRequest,
-    modifier = modifier
-        .width(IntrinsicSize.Min)
-        .height(IntrinsicSize.Min),
-    properties = DialogProperties(usePlatformDefaultWidth = false),
-) {
-    Surface(
-        shape = MaterialTheme.shapes.extraLarge,
-        tonalElevation = 6.dp,
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant) {
-                ProvideTextStyle(MaterialTheme.typography.labelLarge) {
-                    Box(
-                        modifier = Modifier
-                            .align(Alignment.Start)
-                            .padding(horizontal = 24.dp)
-                            .padding(top = 16.dp, bottom = 20.dp),
-                    ) { title() }
-                }
-            }
-
-            CompositionLocalProvider(value = LocalContentColor provides AlertDialogDefaults.textContentColor) {
-                content()
-            }
-
-            CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.primary) {
-                ProvideTextStyle(MaterialTheme.typography.labelLarge) {
-                    // TODO This should wrap on small screens, but we can't use AlertDialogFlowRow as it is no public
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 8.dp)
-                            .padding(horizontal = 6.dp),
-                        content = buttons,
-                    )
-                }
-            }
         }
     }
 }
