0bbe41fe522c600b9dbea5f5d9c7e570fb91c004
Ilya Solovyov <mail@erdenian.com>
2026-01-29 00:41:42 +0300

    Убрано подключение плагина kotlin-android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ import org.gradle.internal.extensions.stdlib.capitalized
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
